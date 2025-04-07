ae81fe28547487892bc257b02a88afddf1f8e585
Christoph Zirkelbach <christoph@tigion.de>
2025-04-07 10:48:18 +0200

    refactor(lua/number): format code

--- lua/number/main.lua
+++ lua/number/main.lua
@@ -6,7 +6,7 @@ local attempts = 0 -- The number of attempts.
 local secret_number = math.random(min_number, max_number)
 
 -- Prints the intro message.
-print("Guess my number between " .. min_number .. " and " .. max_number .. "!\n")
+print('Guess my number between ' .. min_number .. ' and ' .. max_number .. '!\n')
 
 -- The main loop.
 while true do
@@ -14,25 +14,25 @@ while true do
   attempts = attempts + 1
 
   -- Asks for the input (without a line break).
-  io.write("Your guess: ")
+  io.write('Your guess: ')
   -- Converts the input to an integer (base 10).
   local guess = tonumber(io.read(), 10)
 
   -- Checks if the guess is a valid number, correct, to low or to high.
   if not guess then
     -- Is not a valid number.
-    print("Invalid input!")
+    print('Invalid input!')
   elseif guess == secret_number then
     -- The guess is correct.
-    print("\nYou win, the number was " .. secret_number .. "!")
-    print("You had " .. attempts .. " attempts.")
+    print('\nYou win, the number was ' .. secret_number .. '!')
+    print('You had ' .. attempts .. ' attempts.')
     -- Exits the loop.
     break
   elseif guess < secret_number then
     -- The guess is too low.
-    print("Too low!")
+    print('Too low!')
   elseif guess > secret_number then
     -- The guess is too high.
-    print("Too high!")
+    print('Too high!')
   end
 end
