295973177ad1ac02f13cbf0942c0f227b33cd7a0
Christoph Zirkelbach <christoph@tigion.de>
2026-07-21 16:37:43 +0200

    refactor(c/number): extract the user input into a separate function

--- c/number/main.c
+++ c/number/main.c
@@ -1,7 +1,30 @@
-// Includes the standard libraries.
+// Include the standard libraries.
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/**
+ * Reads and returns a valid integer guess from the user.
+ *
+ * NOTE: The number 0 is a special case here.
+ */
+int readGuess() {
+  char input[256];
+
+  while (1) {
+    printf("Your guess: ");
+    fgets(input, 256, stdin);
+
+    // Convert the input to an integer.
+    // If the input is not a valid number, it will return 0.
+    int guess = atoi(input);
+
+    // Check if the guess is valid and return it.
+    if (guess != 0) return guess;
+
+    printf("Invalid input!\n");
+  }
+}
 
 /**
  * The main function.
@@ -10,41 +33,31 @@ int main(int argc, char *argv[]) {
   int minNumber = 1;   // The minimum number.
   int maxNumber = 100; // The maximum number.
   int attempts = 0;    // The number of attempts.
-  char input[256];     // The input buffer.
 
-  // Sets the seed for the `rand` function.
+  // Set the seed for the `rand` function.
   srand(time(NULL));
-  // Generates a random number between the minimum and maximum.
+  // Generate a random number between the minimum and maximum.
   int secretNumber = rand() % (maxNumber - minNumber + 1) + minNumber;
 
   printf("Guess my number between %d and %d!\n\n", minNumber, maxNumber);
 
   // The main loop.
   while (1) {
-    // Increases the number of attempts.
+    // Read the user's guess.
+    int guess = readGuess();
+
+    // Increase the number of attempts.
     attempts++;
 
-    // Asks for the input and checks if it is a valid number.
-    printf("Your guess: ");
-    fgets(input, 256, stdin);
-    int guess = atoi(input);
-    if (guess == 0) {
-      printf("Invalid input!\n");
-      continue;
-    }
-
-    // Checks if the guess is correct, to low or to high.
-    if (guess == secretNumber) {
-      // The guess is correct.
+    // Check if the guess is to low, to high or correct.
+    if (guess < secretNumber) {
+      printf("Too low!\n");
+    } else if (guess > secretNumber) {
+      printf("Too high!\n");
+    } else {
       printf("\nYou win, the number was %d!\n", secretNumber);
       printf("You had %d attempts.\n", attempts);
       break;
-    } else if (guess < secretNumber) {
-      // The guess is too low.
-      printf("Too low!\n");
-    } else {
-      // The guess is too high.
-      printf("Too high!\n");
     }
   }
 
