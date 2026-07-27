825c8839048d42c3600c8ba3baf35c6848ee109a
Christoph Zirkelbach <christoph@tigion.de>
2026-07-27 12:24:30 +0200

    style(php/number): format code

--- php/number/main.php
+++ php/number/main.php
@@ -8,26 +8,26 @@ declare(strict_types=1);
  */
 function readGuess(): int
 {
-  while (true) {
-    echo 'Your guess: ';
+    while (true) {
+        echo 'Your guess: ';
 
-    $input = fgets(STDIN);
+        $input = fgets(STDIN);
 
-    // Exit cleanly if input stream is closed (e.g., Ctrl + D).
-    if ($input === false) {
-      exit(0);
+        // Exit cleanly if input stream is closed (e.g., Ctrl + D).
+        if ($input === false) {
+            exit(0);
+        }
+
+        // Trim whitespace and newline characters from the input.
+        $text = trim($input);
+
+        // Validate that the input is an integer.
+        if (filter_var($text, FILTER_VALIDATE_INT) !== false) {
+            return (int) $text;
+        }
+
+        echo "Invalid input!\n";
     }
-
-    // Trim whitespace and newline characters from the input.
-    $text = trim($input);
-
-    // Validate that the input is an integer.
-    if (filter_var($text, FILTER_VALIDATE_INT) !== false) {
-      return (int) $text;
-    }
-
-    echo "Invalid input!\n";
-  }
 }
 
 /**
@@ -35,34 +35,34 @@ function readGuess(): int
  */
 function main(): void
 {
-  $minNumber = 1;
-  $maxNumber = 100;
-  $attempts = 0;
+    $minNumber = 1;
+    $maxNumber = 100;
+    $attempts = 0;
 
-  // Generate a random number between the minimum and maximum.
-  $secretNumber = random_int($minNumber, $maxNumber);
+    // Generate a random number between the minimum and maximum.
+    $secretNumber = random_int($minNumber, $maxNumber);
 
-  echo "Guess my number between {$minNumber} and {$maxNumber}!\n\n";
+    echo "Guess my number between {$minNumber} and {$maxNumber}!\n\n";
 
-  // The main loop.
-  while (true) {
-    // Read the guess from the user.
-    $guess = readGuess();
+    // The main loop.
+    while (true) {
+        // Read the guess from the user.
+        $guess = readGuess();
 
-    // Increase the number of attempts.
-    $attempts++;
+        // Increase the number of attempts.
+        $attempts++;
 
-    // Check if the guess is too low, too high or correct.
-    if ($guess < $secretNumber) {
-      echo "Too low!\n";
-    } elseif ($guess > $secretNumber) {
-      echo "Too high!\n";
-    } else {
-      echo "\nYou win, the number was {$secretNumber}!\n";
-      echo "You had {$attempts} attempts.\n";
-      break;
+        // Check if the guess is too low, too high or correct.
+        if ($guess < $secretNumber) {
+            echo "Too low!\n";
+        } elseif ($guess > $secretNumber) {
+            echo "Too high!\n";
+        } else {
+            echo "\nYou win, the number was {$secretNumber}!\n";
+            echo "You had {$attempts} attempts.\n";
+            break;
+        }
     }
-  }
 }
 
 // Start the program.
